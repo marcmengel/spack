3e1e846a92da73492dba0a96a4a98a3cddfbe26b
Chris Green <artists@fnal.gov>
2018-06-06 15:50:53 -0500

    Allow variants for different language standards.

--- var/spack/repos/builtin/packages/cppunit/package.py
+++ var/spack/repos/builtin/packages/cppunit/package.py
@@ -32,3 +32,31 @@ class Cppunit(AutotoolsPackage):
     url = "http://dev-www.libreoffice.org/src/cppunit-1.13.2.tar.gz"
 
     version('1.13.2', '0eaf8bb1dcf4d16b12bec30d0732370390d35e6f')
+
+    variant('cxxstd',
+            default='default',
+            values=('default', '98', '11', '14', '17'),
+            multi=False,
+            description='Use the specified C++ standard when building.')
+
+    def setup_environment(self, spack_env, run_env):
+        cxxstd = self.spec.variants['cxxstd'].value
+        cxxstdflag = ''
+        if cxxstd == '98':
+            cxxstdflag = self.compiler.cxx98_flag
+        elif cxxstd == '11':
+            cxxstdflag = self.compiler.cxx11_flag
+        elif cxxstd == '14':
+            cxxstdflag = self.compiler.cxx14_flag
+        elif cxxstd == '17':
+            cxxstdflag = self.compiler.cxx17_flag
+        elif cxxstd == 'default':
+            pass
+        else:
+            # The user has selected a (new?) legal value that we've
+            # forgotten to deal with here.
+            tty.die(
+                "INTERNAL ERROR: cannot accommodate unexpected variant ",
+                "cxxstd={0}".format(spec.variants['cxxstd'].value))
+
+        spack_env.append_flags('CXXFLAGS', cxxstdflag)
