6342a179d7c6a00e5e02f0f7b8acbaf7e1b51d73
Chris Green <artists@fnal.gov>
2019-06-18 14:45:58 -0500

    jsoncpp: new versions, C++ standard support.

--- var/spack/repos/builtin/packages/jsoncpp/package.py
+++ var/spack/repos/builtin/packages/jsoncpp/package.py
@@ -15,6 +15,15 @@ class Jsoncpp(CMakePackage):
     homepage = "https://github.com/open-source-parsers/jsoncpp"
     url      = "https://github.com/open-source-parsers/jsoncpp/archive/1.7.3.tar.gz"
 
+    version('1.8.4',  sha256='c49deac9e0933bcb7044f08516861a2d560988540b23de2ac1ad443b219afdb6')
+    version('1.8.3',  sha256='3671ba6051e0f30849942cc66d1798fdf0362d089343a83f704c09ee7156604f')
+    version('1.8.2',  sha256='811f5aee20df2ef0868a73a976ec6f9aab61f4ca71c66eddf38094b2b3078eef')
+    version('1.8.1',  sha256='858db2faf348f89fdf1062bd3e79256772e897e7f17df73e0624edf004f2f9ac')
+    version('1.8.0',  sha256='5deb2462cbf0c0121c9d6c9823ec72fe71417e34242e3509bc7c003d526465bc')
+    version('1.7.7',  sha256='087640ebcf7fbcfe8e2717a0b9528fff89c52fcf69fa2a18cc2b538008098f97')
+    version('1.7.6',  sha256='07cf5d4f184394ec0a9aa657dd4c13ea682c52a1ab4da2fb176cb2d5501101e8')
+    version('1.7.5',  sha256='4338c6cab8af8dee6cdfd54e6218bd0533785f552c6162bb083f8dd28bf8fbbe')
+    version('1.7.4',  sha256='10dcd0677e80727e572a1e462193e51a5fde3e023b99e144b2ee1a469835f769')
     version('1.7.3', 'aff6bfb5b81d9a28785429faa45839c5')
 
     variant('build_type', default='RelWithDebInfo',
@@ -22,8 +31,18 @@ class Jsoncpp(CMakePackage):
             values=('Debug', 'Release', 'RelWithDebInfo',
                     'MinSizeRel', 'Coverage'))
 
+    variant('cxxstd',
+            default='default',
+            values=('default', '98', '11', '14', '17'),
+            multi=False,
+            description='Use the specified C++ standard when building.')
+
     depends_on('cmake@3.1:', type='build')
     depends_on('python', type='test')
 
     def cmake_args(self):
-        return ['-DBUILD_SHARED_LIBS=ON']
+        args = ['-DBUILD_SHARED_LIBS=ON']
+        cxxstd = self.spec.variants['cxxstd'].value
+        if cxxstd != 'default':
+            args.append('-DCMAKE_CXX_STANDARD={0}'.format(cxxstd))
+        return args
