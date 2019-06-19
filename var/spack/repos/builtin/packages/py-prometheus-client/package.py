e43f4901963abffc3281e94efd46261715deac7c
Marc Mengel <mengel@fnal.gov>
2019-06-19 10:30:35 -0500

    patch to prometheus recipe

--- var/spack/repos/builtin/packages/py-prometheus-client/package.py
+++ var/spack/repos/builtin/packages/py-prometheus-client/package.py
@@ -12,6 +12,6 @@ class PyPrometheusClient(PythonPackage):
     homepage = "https://pypi.org/project/prometheus_client/"
     url      = "https://pypi.io/packages/source/p/prometheus_client/prometheus_client-0.5.0.tar.gz"
 
-    version('0.5.0', sha256='2d7f7af343dec0a96ee849b1bba18aad9f767bf16d5eb5f0c11cae837bf22731')
+    version('0.5.0', sha256='e8c11ff5ca53de6c3d91e1510500611cafd1d247a937ec6c588a0a7cc3bef93c')
 
     depends_on('py-setuptools', type='build')
