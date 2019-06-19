f7f69571875f132e01b9985819ca702f2ab55a44
Marc Mengel <mengel@fnal.gov>
2019-06-19 13:43:34 -0500

    dependency fun

--- var/spack/repos/builtin/packages/py-poms/package.py
+++ var/spack/repos/builtin/packages/py-poms/package.py
@@ -8,17 +8,16 @@ class PyPoms(PythonPackage):
     url = 'http://cdcvs.fnal.gov/cgi-bin/git_archive.cgi/cvs/projects/prod_mgmt_db.v4_1_0.tar'
 
     # FIXME: Add proper versions and checksums here.
-    version('4_1_2', '9940e7021614dbcc0b0d54056455fccd')
+    version('4_1_2', '8c44623b2fcc78ac2120fb5a185917de')
     version('4_1_0', 'c8682135a0d4eae8df00a29ececb6020')
 
     depends_on('python',               type=('build','run'))
-    depends_on('py-setuptools',        type='build')
+    depends_on('py-setuptools',        type=('build', 'run'))
     depends_on('py-psycopg2',          type=('build', 'run'))
     depends_on('py-cherrypy',          type=('build', 'run'))
     depends_on('py-sqlalchemy',        type=('build', 'run'))
     depends_on('py-jinja2',            type=('build', 'run'))
     depends_on('py-python-crontab',    type=('build', 'run'))
     depends_on('py-futures',           type=('build', 'run'))
-    depends_on('py-more-itertools',    type=('build', 'run'))
     depends_on('py-prometheus-client', type=('build', 'run'))
 
