2fc1cd885e7dee09b308f151537dae8bd4b9fb7a
Marc Mengel <mengel@fnal.gov>
2019-10-04 14:02:13 -0500

    python 2.6 fix

--- lib/spack/spack/modules/ups_table.py
+++ lib/spack/spack/modules/ups_table.py
@@ -66,7 +66,7 @@ class UpsTableFileLayout(BaseFileLayout):
     @property
     def filename(self):
         """Name of the module file for the current spec."""
-        fn = "{}-{}-{}.table".format(self.spec.name, self.spec.version, self.spec._hash)
+        fn = "{0}-{1}-{2}.table".format(self.spec.name, self.spec.version, self.spec._hash)
         fp = os.path.join(self.dirname(), fn)
         return  fp
 
