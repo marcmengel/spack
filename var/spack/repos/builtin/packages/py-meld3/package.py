bcff11426f4149f4bce4159c4221921700fdaecd
Marc Mengel <mengel@fnal.gov>
2019-10-25 16:56:25 -0500

    package py-meld3

---
+++ var/spack/repos/builtin/packages/py-meld3/package.py
@@ -0,0 +1,14 @@
+
+from spack import *
+
+
+class PyMeld3(PythonPackage):
+    """meld3 is an HTML/XML templating engine."""
+
+    homepage = "https://github.com/supervisor/meld3"
+    url      = "https://files.pythonhosted.org/packages/45/a0/317c6422b26c12fe0161e936fc35f36552069ba8e6f7ecbd99bbffe32a5f/meld3-1.0.2.tar.gz"
+
+    version('1.0.2', sha256='f7b754a0fde7a4429b2ebe49409db240b5699385a572501bb0d5627d299f9558')
+
+    depends_on('py-setuptools', type='build')
+
