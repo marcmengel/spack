5738dfbab404dae9dd501d3c971db44e6c8ae51d
Marc Mengel <mengel@fnal.gov>
2019-10-25 16:56:26 -0500

    package py-pyopenssl

--- var/spack/repos/builtin/packages/py-pyopenssl/package.py
+++ var/spack/repos/builtin/packages/py-pyopenssl/package.py
@@ -1,10 +1,4 @@
-# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
-#
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
 from spack import *
-
 
 class PyPyopenssl(PythonPackage):
     """High-level wrapper around a subset of the OpenSSL library.
