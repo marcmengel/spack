47f3063f05dc073763a2ad37bed55fe533753e1b
Marc Mengel <mengel@fnal.gov>
2019-11-05 16:33:47 -0600

    autopep8

--- var/spack/repos/builtin/packages/py-supervisord/package.py
+++ var/spack/repos/builtin/packages/py-supervisord/package.py
@@ -13,7 +13,8 @@ class PySupervisord(PythonPackage):
     homepage = "http://supervisord"
     url      = "https://files.pythonhosted.org/packages/97/48/f38bf70bd9282d1a18d591616557cc1a77a1c627d57dff66ead65c891dc8/supervisor-4.0.3.tar.gz"
 
-    version('4.0.3', sha256='f768abc073e8702892718938b8a0ab98ebcb91c2afcb39bf2cb570d3eb51149e')
+    version(
+        '4.0.3', sha256='f768abc073e8702892718938b8a0ab98ebcb91c2afcb39bf2cb570d3eb51149e')
 
     depends_on('py-setuptools', type='build')
     depends_on('py-meld3',      type=('build', 'run'))
